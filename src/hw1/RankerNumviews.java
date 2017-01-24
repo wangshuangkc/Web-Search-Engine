0db974533fe350a1fb35e766dc9c66e5eacbf523
wangshuangkc <sw2553@nyu.edu>
2017-01-24 15:30:43 -0500

    ranker numviews done

--- src/hw1/RankerNumviews.java
+++ src/hw1/RankerNumviews.java
@@ -1,5 +1,6 @@
 package hw1;
 
+import java.util.Collections;
 import java.util.Vector;
 
 import hw1.QueryHandler.CgiArguments;
@@ -22,8 +23,27 @@ public class RankerNumviews extends Ranker {
   @Override
   public Vector<ScoredDocument> runQuery(Query query, int numResults) {
     Vector<ScoredDocument> all = new Vector<ScoredDocument>();
-    // @CS2580: fill in your code here.
-    return all;
+    for (int i = 0; i < _indexer.numDocs(); ++i) {
+      all.add(scoreDocument(query, i));
+    }
+    Collections.sort(all, Collections.reverseOrder());
+    Vector<ScoredDocument> results = new Vector<ScoredDocument>();
+    for (int i = 0; i < all.size() && i < numResults; ++i) {
+      results.add(all.get(i));
+    }
+    return results;
+  }
+
+  private ScoredDocument scoreDocument(Query query, int did)
+  {
+    Document doc = _indexer.getDoc(did);
+    double score = scoreDocument(doc);
+
+    return new ScoredDocument(query._query, doc, score);
+  }
+
+  private double scoreDocument(Document doc) {
+    return doc.getNumViews();
   }
 }
 
