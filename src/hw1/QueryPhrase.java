9cabddfaa2f5241c48125c6cf22a797091c455b1
wangshuangkc <sw2553@nyu.edu>
2017-01-24 15:27:07 -0500

    ranker phrase done

--- src/hw1/QueryPhrase.java
+++
@@ -1,7 +0,0 @@
-package hw1;
-
-/**
- * Created by kc on 1/20/17.
- */
-public class QueryPhrase {
-}
--- src/hw1/RankerPhrase.java
+++ src/hw1/RankerPhrase.java
@@ -1,5 +1,7 @@
 package hw1;
 
+import java.io.IOException;
+import java.util.Collections;
 import java.util.Vector;
 
 import hw1.QueryHandler.CgiArguments;
@@ -21,9 +23,55 @@ public class RankerPhrase extends Ranker {
 
   @Override
   public Vector<ScoredDocument> runQuery(Query query, int numResults) {
-    Vector<ScoredDocument> all = new Vector<ScoredDocument>();
-    // @CS2580: fill in your code here.
-    return all;
+    Vector<ScoredDocument> all = new Vector<>();
+    for (int i = 0; i < _indexer.numDocs(); ++i) {
+      all.add(scoreDocument(query, i));
+    }
+    Collections.sort(all, Collections.reverseOrder());
+    Vector<ScoredDocument> results = new Vector<>();
+    for (int i = 0; i < all.size() && i < numResults; ++i) {
+      results.add(all.get(i));
+    }
+    return results;
+  }
+
+  private ScoredDocument scoreDocument(Query query, int did)
+  {
+    Document doc = _indexer.getDoc(did);
+    double score = scoreDocument(query, doc);
+
+    return new ScoredDocument(query._query, doc, score);
+  }
+
+  public double scoreDocument(Query query, Document doc) {
+    double score = 0;
+    Vector<String> token = ((DocumentFull) doc).getConvertedBodyTokens();
+    if (query._tokens.size() == 1) {
+      for (String q : query._tokens) {
+        for (String t : token) {
+          if (q.equals(t)) {
+            score++;
+          }
+        }
+      }
+    } else {
+      for (int i = 0; i < query._tokens.size() - 1; i++) {
+        if (token.size() == 1) {
+          if (token.get(0).equals(query._tokens.get(i))) {
+            score++;
+          }
+        } else {
+          for (int j = 0; j < token.size() - 1; j++) {
+            if (query._tokens.get(i).equals(token.get(j)) &&
+                query._tokens.get(i + 1).equals(token.get(j + 1))) {
+              score++;
+            }
+          }
+        }
+      }
+    }
+
+    return score;
   }
 }
 
